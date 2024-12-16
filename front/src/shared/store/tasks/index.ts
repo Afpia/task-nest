e54737cf088e7ee67209f19db39fc2e57d04eb20
Nikita Galkin <nikiprostoff@gmail.com>
2024-12-16 18:09:07 +0500

    feat(tasks): Добавление store tasks вывода всех задач на проекте

--- front/src/shared/store/tasks/index.ts
+++ front/src/shared/store/tasks/index.ts
@@ -1,12 +1,15 @@
-import { createEffect, createStore } from 'effector'
+import { createEffect, createStore, sample } from 'effector'
 
 import { deleteTaskProject, getTasksProject, postTaskProject, putTaskStatusProject } from '@shared/api'
+import { privateRouteOpened, routes } from '@shared/config'
 import type { PostTaskProjectConfig, TasksResponse } from '@shared/types'
+
+import { $currentProject } from '../projects'
 
 export const $tasks = createStore([] as TasksResponse)
 export const $tasksUser = createStore([] as TasksResponse)
 
-export const getTasksProjectFx = createEffect((projectId: string) => getTasksProject({ params: { projectId } }))
+export const getTasksProjectFx = createEffect((projectId: number) => getTasksProject({ params: { projectId } }))
 export const getUserTasksProjectFx = createEffect()
 
 export const postTaskProjectFx = createEffect(({ params, data }: PostTaskProjectConfig) => postTaskProject({ params, data }))
@@ -14,3 +17,11 @@ export const postTaskProjectFx = createEffect(({ params, data }: PostTaskProject
 export const putTaskStatusProjectFx = createEffect((taskId: string) => putTaskStatusProject({ params: { taskId } }))
 
 export const deleteTaskProjectFx = createEffect((taskId: string) => deleteTaskProject({ params: { taskId } }))
+
+sample({
+	clock: $currentProject,
+	source: $tasks,
+	filter: $tasks.map((tasks) => !tasks.length),
+	fn: (_, clk) => clk.project.id,
+	target: getTasksProjectFx
+})
--- front/src/widgets/total/ui.tsx
+++ front/src/widgets/total/ui.tsx
@@ -2,18 +2,14 @@ import { useUnit } from 'effector-react'
 
 import { Box, Divider, Flex, Text, Title, useMantineTheme } from '@mantine/core'
 
-import { $projects } from '@shared/store'
-
-import { $countProjects } from './model'
-
-import styles from './ui.module.css'
+import { $projects, $tasks, $tasksUser } from '@shared/store'
 
 export const TotalStats = () => {
 	const theme = useMantineTheme()
-	const [countProjects] = useUnit([$projects])
+	const [countProjects, countTasks, countUserTasks] = useUnit([$projects, $tasks, $tasksUser])
 
 	return (
-		<Box className={styles.root} bd={`1px solid ${theme.colors.gray[3]}`}>
+		<Box style={{ borderRadius: '16px' }} w='100%' h='120px' bd={`1px solid ${theme.colors.gray[3]}`}>
 			<Flex w='100%' gap={30} p={20} align='center' h='100%'>
 				<Flex w='20%' justify='space-between'>
 					<Flex direction='column' align='start' h='100%' justify='center' gap={10}>
@@ -31,8 +27,8 @@ export const TotalStats = () => {
 						<Title order={2} size={18} fw={600} c={theme.colors.gray[6]}>
 							Всего задач
 						</Title>
-						<Text size='30px' c={theme.colors.gray[6]}>
-							0
+						<Text size='30px' c={countTasks.length === 0 ? `${theme.colors.gray[6]}` : ''}>
+							{countTasks.length}
 						</Text>
 					</Flex>
 					<Divider orientation='vertical' variant='dashed' size={2} color={theme.colors.gray[3]} />
@@ -64,8 +60,8 @@ export const TotalStats = () => {
 						<Title order={2} size={18} fw={600} c={theme.colors.gray[6]}>
 							Мои задачи
 						</Title>
-						<Text size='30px' c={theme.colors.gray[6]}>
-							0
+						<Text size='30px' c={countUserTasks.length === 0 ? `${theme.colors.gray[6]}` : ''}>
+							{countUserTasks.length}
 						</Text>
 					</Flex>
 				</Flex>
