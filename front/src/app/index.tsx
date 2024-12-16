4721d5bcde7ae2a88898f015dac3f5917251ba0a
Nikita Galkin <nikiprostoff@gmail.com>
2024-12-16 19:33:19 +0500

    feat(progress): Добавление прогресс бара при запросах
    Убрал баг сохранении стейта при создании нового проекта

--- front/src/app/index.tsx
+++ front/src/app/index.tsx
@@ -3,14 +3,16 @@ import { RouterProvider } from 'atomic-router-react'
 import { createTheme, MantineProvider, type MantineTheme } from '@mantine/core'
 import { ModalsProvider } from '@mantine/modals'
 import { Notifications } from '@mantine/notifications'
+import { NavigationProgress } from '@mantine/nprogress'
 
 import { Pages } from '@pages/index'
-import { myColors, router, started } from '@shared/config'
+import { myColors, router } from '@shared/config'
 
 import '@mantine/notifications/styles.css'
 import '@mantine/spotlight/styles.css'
 import '@mantine/dropzone/styles.css'
 import '@mantine/core/styles.css'
+import '@mantine/nprogress/styles.css'
 import '@assets/styles/global.css'
 
 const theme = createTheme({
@@ -67,5 +69,6 @@ export const App = () => (
 			</RouterProvider>
 			{/* </AuthProvider> */}
 		</ModalsProvider>
+		<NavigationProgress />
 	</MantineProvider>
 )
