44310f40f0cd16a03231e0920234aa44e35f0444
Nikita Galkin <nikiprostoff@gmail.com>
2024-12-16 16:19:55 +0500

    refactor(add-project): Рефакторинг виджета добавление проектов

--- front/src/widgets/add-projects/ui/ui.tsx
+++ front/src/widgets/add-projects/ui/ui.tsx
@@ -17,24 +17,24 @@ import { ActionIcon, Box, Divider, Flex, Grid, ScrollArea, Skeleton, Title } fro
 import { useDisclosure } from '@mantine/hooks'
 
 import { ModalCreateProject } from '@entities/modal.create.project'
-import { $projects, getProjectsWorkspaceFx } from '@shared/store'
+import { ThemeColors } from '@shared/config'
+import { isDarkMode } from '@shared/helpers'
+import { getProjectsWorkspaceFx } from '@shared/store'
+
+import { $activeProject, $projectsWidget, changedActiveProject, changedPositionItem } from '../model'
 
 import { SortableItem } from './sortable-item'
 
 export const AddProjects = () => {
-	const [projects, loading] = useUnit([$projects, getProjectsWorkspaceFx.pending])
+	const [loading, projects, changePosition, activeProject, changeActive] = useUnit([
+		getProjectsWorkspaceFx.pending,
+		$projectsWidget,
+		changedPositionItem,
+		$activeProject,
+		changedActiveProject
+	])
+	const { isDark } = isDarkMode()
 	const [opened, { open, close }] = useDisclosure(false)
-
-	const [activeTask, setActiveTask] = useState<number | string | null>(null)
-
-	const [items, setItems] = useState(projects)
-
-	useEffect(() => {
-		setItems([
-			{ id: -1, title: 'Добавление проекта', description: '', start_date: '', status: '', remaining_days: 0, image_url: '' },
-			...projects
-		])
-	}, [projects])
 
 	const sensors = useSensors(
 		useSensor(PointerSensor),
@@ -43,18 +43,16 @@ export const AddProjects = () => {
 		})
 	)
 
-	const getTaskPos = (id: number | string) => items.findIndex((item) => item.id === id)
+	const getTaskPos = (id: number | string) => projects.findIndex((item) => item.id === id)
 
 	function handleDragEnd(event: DragEndEvent) {
 		const { active, over } = event
 
 		if (over !== null && active.id !== over.id) {
-			setItems((item) => {
-				const oldIndex = getTaskPos(active.id)
-				const newIndex = getTaskPos(over.id)
+			const oldIndex = getTaskPos(active.id)
+			const newIndex = getTaskPos(over.id)
 
-				return arrayMove(item, oldIndex, newIndex)
-			})
+			changePosition(arrayMove(projects, oldIndex, newIndex))
 		}
 	}
 
@@ -65,7 +63,7 @@ export const AddProjects = () => {
 			w='50%'
 			h='350px'
 			bd='1px solid #D9D9D9'
-			bg='#fff'
+			bg={isDark ? ThemeColors.dark : ThemeColors.light}
 		>
 			<Flex justify='space-between' pr={15} h={36}>
 				<Title order={2} size={20} fw={600}>
@@ -79,27 +77,28 @@ export const AddProjects = () => {
 			<ScrollArea scrollbars='y' h='270px'>
 				<DndContext
 					sensors={sensors}
-					onDragStart={(event) => setActiveTask(event.active.id)}
+					onDragStart={(event) => changeActive(event.active.id)}
 					collisionDetection={closestCorners}
 					onDragEnd={handleDragEnd}
 				>
 					<Grid h='100%' styles={{ inner: { maxWidth: '100%' } }}>
-						{items && (
-							<SortableContext items={items} strategy={rectSortingStrategy}>
-								{items.map((item) => (
+						{projects && (
+							<SortableContext items={projects} strategy={rectSortingStrategy}>
+								{projects.map((item) => (
 									<Grid.Col span={4} key={item.id}>
 										<SortableItem {...item} open={open} />
 									</Grid.Col>
 								))}
 								<DragOverlay>
-									{items
-										.filter((item) => item.id === activeTask)
+									{projects
+										.filter((item) => item.id === activeProject)
 										.map((item) => (
 											<SortableItem key={item.id} {...item} />
 										))}
 								</DragOverlay>
 							</SortableContext>
 						)}
+						{/* eslint-disable-next-line style/operator-linebreak */}
 						{loading &&
 							Array.from({ length: 8 }).map((_, index) => (
 								// eslint-disable-next-line react/no-array-index-key
@@ -119,9 +118,13 @@ export const AddProjects = () => {
 					pointerEvents: 'none',
 					left: 0,
 					right: 0,
-					height: '60px',
-					background: 'linear-gradient(to top, white, rgba(255, 255, 255, 0))'
+					height: '60px'
 				}}
+				bg={
+					!isDark
+						? `linear-gradient(to top, ${ThemeColors.light}, ${ThemeColors.thirdLight})`
+						: `linear-gradient(to top, ${ThemeColors.dark}, ${ThemeColors.thirdDark})`
+				}
 			/>
 		</Box>
 	)
